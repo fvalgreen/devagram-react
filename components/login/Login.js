b9be04c046b6069f046ef5cf61f9a9a07e68b629
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-03-25 18:19:57 -0300

    Tela de Login

---
+++ components/login/Login.js
@@ -0,0 +1,54 @@
+import InputPublico from "../inputPublico";
+import imagemEnvelope from '../../public/imagens/envelope.svg';
+import imagemChave from '../../public/imagens/chave.svg';
+import imagemLogo from '../../public/imagens/logo.svg';
+import Image from "next/image";
+import Botao from "../botao";
+import Link from "next/link";
+import { use, useState } from "react";
+
+export default function Login(){
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <section className={`paginaLogin paginaPublica`}>
+      <div className="logoContainer">
+        <Image src={imagemLogo}
+          alt='logotipo'
+          layout="fill"
+          className="logo"
+        />
+      </div>
+      <div className="conteudoPaginaPublica">
+        <form>
+          <InputPublico 
+            imagem={imagemEnvelope}
+            texto = 'E-mail'
+            tipo="email"
+            aoAlterarValor={e => setEmail(e.target.value)}
+            valor={email}
+          />
+          <InputPublico 
+            imagem={imagemChave}
+            texto = 'Senha'
+            tipo="password"
+            aoAlterarValor={e => setSenha(e.target.value)}
+            valor={senha}
+          />
+          <Botao 
+            texto="Login"
+            tipo="submit"
+            desabilitado={false}
+          />
+        </form>
+
+        <div className="rodapePaginaPublica">
+          <p>Não possui uma conta?</p>
+          <Link href={"/cadastro"}>Faça seu cadastro agora!</Link>
+        </div>
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Botao from "@/components/botao";
-import Avatar from "@/components/avatar";
-import UploadImagem from "@/components/uploadImagem";
-import { useRef, useState } from "react";
+import Login from "@/components/login/Login";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  const [imagem, setImagem] = useState(null);
-  const referenciaInput = useRef(null);
-
+export default function Index() {
   return (
     <>
-      <h1>Olá, mundo</h1>
-      <button onClick={() => referenciaInput?.current.click()}>
-        abrir seletor de arquivos
-      </button>
-      <UploadImagem
-        setImagem={setImagem}
-        imagemPreview={imagem?.preview}
-        aoSetarAReferencia={(ref) => (referenciaInput.current = ref)}
-      />
-      <div style={{ width: 350 }}>
-        <Avatar />
-        <Botao
-          texto={"Login"}
-          manipularClique={() => console.log("Botão clicado")}
-        />
-      </div>
+      <Login />      
     </>
   );
 }
