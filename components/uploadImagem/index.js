4312bbf35ff5bd01652cd522503025b0ce333684
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-03-25 12:29:34 -0300

    UploadImagem

---
+++ components/uploadImagem/index.js
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from "react";
+
+export default function UploadImagem({ className = '',
+setImagem,
+imagemPreview,
+imagemPreviewClassName,
+aoSetarAReferencia }) {
+  const referenciaInput = useRef(null);
+
+  useEffect(() => {
+    if(!aoSetarAReferencia){
+        return;
+    }
+    aoSetarAReferencia(referenciaInput?.current);
+  }, [referenciaInput?.current])
+  const abrirSeletorArquivo = () => {
+    referenciaInput?.current?.click();
+  };
+
+  const aoAlterarImagem = () => {   
+    if(!referenciaInput?.current?.files.length){
+        return;
+    }
+    
+    const arquivo = referenciaInput?.current?.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(arquivo);
+    fileReader.onloadend = () => {
+        setImagem({
+            preview: fileReader.result,
+            arquivo
+        });
+    }
+  }
+
+  return (
+    <>
+      <div
+        className={`uploadImagemContainer ${className}`}
+        onClick={abrirSeletorArquivo}
+      >
+        
+        {imagemPreview && (
+            <div className="imagemPreviewContainer">
+                <img src={imagemPreview} alt='imagem preview' className={imagemPreviewClassName} />
+            </div>
+        )}
+        <input
+          type="file"
+          className="oculto"
+          accept="image/*"
+          ref={referenciaInput} onChange={aoAlterarImagem}
+        />
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,20 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Botao from "@/components/botao";
 import Avatar from "@/components/avatar";
+import UploadImagem from "@/components/uploadImagem";
+import { useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+  console.log(imagem);
   return (
     <>
       <h1>Olá, mundo</h1>
+      <button onClick={() => referenciaInput?.current.click()}>abrir seletor de arquivos</button>
+      <UploadImagem setImagem={setImagem} imagemPreview={imagem?.preview} aoSetarAReferencia={(ref) => referenciaInput.current = ref}/>
       <div style={{ width: 350 }}>
         <Avatar />
         <Botao
