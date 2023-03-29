e6ef6237fa8bd7b6cd0f0cf1c04efaba3d1fb13b
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-03-28 22:41:12 -0300

    Resultado Pesquisa

--- components/layout/Cabecalho.js
+++ components/layout/Cabecalho.js
@@ -1,36 +1,80 @@
-import Image from 'next/image';
-import logoHorizontal from '../../public/imagens/logoHorizontal.svg';
-import imagemLupa from '../../public/imagens/lupa.svg';
-import Navegacao from './Navegacao';
+import Image from "next/image";
+import { useState } from "react";
+import logoHorizontal from "../../public/imagens/logoHorizontal.svg";
+import imagemLupa from "../../public/imagens/lupa.svg";
+import Navegacao from "./Navegacao";
+import Resultadopesquisa from "./ResultadoPesquisa";
 
-export default function Cabecalho(){
+export default function Cabecalho() {
+  const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+  const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+  const aoPesquisar = (e) => {
+    setTermoPesquisado(e.target.value);
+    setResultadoPesquisa([]);
+    if (termoPesquisado.length < 3) {      
+      return;
+    }
+    setResultadoPesquisa([
+      {
+        avatar: '',
+        nome: 'Felipe',
+        email: 'felipe@gmail.com',
+        _id: '1234565',
+      },
+      {
+        avatar: '',
+        nome: 'João',
+        email: 'joao@gmail.com',
+        _id: '1362455',
+      },
+      {
+        avatar: '',
+        nome: 'Marcos',
+        email: 'marcos@gmail.com',
+        _id: '6123455',
+      },
+    ]);
+  };
+
+  const aoClicarResultadoPesquisa = (id) => {
+    console.log("Ao CLicar no Resultado da Pesquisa", {id});
+  };
+
   return (
-    <header className='cabecalhoPrincipal '>
-      <div className='conteudoCabecalhoPrincipal'>
-        <div className='logoCabecalhoPrincipal'>
-          <Image 
-            src={logoHorizontal}
-            alt='Logo Devagram'
-            layout='fill'
-          />
+    <header className="cabecalhoPrincipal ">
+      <div className="conteudoCabecalhoPrincipal">
+        <div className="logoCabecalhoPrincipal">
+          <Image src={logoHorizontal} alt="Logo Devagram" layout="fill" />
         </div>
-        <div className='barraPesquisa'>
-          <div className='containerImagemLupa'>
-            <Image 
-              src={imagemLupa}
-              alt='Lupa de pesquisa'
-              layout='fill'
-            />
+        <div className="barraPesquisa">
+          <div className="containerImagemLupa">
+            <Image src={imagemLupa} alt="Lupa de pesquisa" layout="fill" />
           </div>
-          <input 
-            type='text'
-            placeholder='Pesquisar'
-            value={''}
-            onChange={() => console.log('Pesquisando')}  
+          <input
+            type="text"
+            placeholder="Pesquisar"
+            value={termoPesquisado}
+            onChange={aoPesquisar}
           />
         </div>
-        <Navegacao className='desktop' />
+        <Navegacao className="desktop" />
       </div>
+
+      {resultadoPesquisa.length > 0 && (
+        <div className="resultadoPesquisaContainer">
+          {resultadoPesquisa.map((r) => (
+            <Resultadopesquisa
+              avatar={r.avatar}
+              nome={r.nome}
+              email={r.email}
+              key={r._id}
+              id={r._id}
+              onClick={aoClicarResultadoPesquisa}
+            />
+          ))}
+        </div>
+      )}
     </header>
   );
-};
+}
