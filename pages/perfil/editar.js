6195726f1a98226a6fca3b51e293f871d0148481
Felipe Valverde <62726786+fvalgreen@users.noreply.github.com>
2023-04-06 15:48:49 -0300

    Editar usuario

--- pages/perfil/editar.js
+++ pages/perfil/editar.js
@@ -2,22 +2,67 @@ import CabecalhoComAcoes from "@/components/cabecalhoComAcoes";
 import UploadImagem from "@/components/uploadImagem";
 import comAutorizacao from "@/hoc/comAutorizacao"
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import imgAvatarPadrao from "../../public/imagens/avatar.svg"
 import Image from "next/image";
 import imgLimpar from "../../public/imagens/limpar.svg"
+import UsuarioService from "@/services/UsuarioService";
+import { validarNome } from "@/utils/validadores";
 
-function EditarPerfil(){
-  const router= useRouter();
+const usuarioService = new UsuarioService();
+
+function EditarPerfil({usuarioLogado}){
+  const router = useRouter();
   const [avatar, setAvatar] = useState();
   const [nome, setNome] = useState('')
   const [inputAvatar, setInputAvatar] = useState();
+
+  useEffect(() => {
+    if(!usuarioLogado){
+      return;
+    }
+    setNome(usuarioLogado.nome);
+    setAvatar({
+      preview: usuarioLogado.avatar
+    });
+  },[])
+
+  const atualizarPerfil = async () => {
+    try {
+      if(!validarNome(nome)){
+        alert('Nome precisa de pelo menos 2 caracteres');
+        return;
+      }
+
+      const corpoRequisicao = new FormData();
+      corpoRequisicao.append('nome', nome);
+      console.log(avatar.arquivo);
+      if(avatar.arquivo){
+        corpoRequisicao.append('file', avatar.arquivo);
+        console.log(corpoRequisicao);
+      };
+      
+
+      await usuarioService.atualizarPerfil(corpoRequisicao);
+
+      localStorage.setItem('nome', nome);
+      if(avatar.arquivo){
+        localStorage.setItem('avatar', avatar.preview)
+      }
+
+
+      router.push('/perfil/eu')
+    } catch (e) {
+      alert(e + ' erro ao editar perfil')
+    }
+  }
+
   const aoCancelarEdicao = () => {
     router.push('/perfil/eu')
   }
 
   const abrirSeletorDeArquivos = () => {
-    console.log('abrir seletor de arquivos')
+    inputAvatar?.click();
   }
 
   
@@ -30,7 +75,7 @@ function EditarPerfil(){
           aoClicarAcaoEsquerda={aoCancelarEdicao}
           textoEsquerda={'Cancelar'}
           elementoDireita={'Concluir'}
-          aoClicarElementoDireita={() => console.log('clicou elemento direita')}
+          aoClicarElementoDireita={atualizarPerfil}
         />
         
         <hr className="linhaDivisoria"/>
@@ -39,7 +84,7 @@ function EditarPerfil(){
             setImagem={setAvatar}
             imagemPreview={avatar?.preview || imgAvatarPadrao.src}
             imagemPreviewClassName={'avatar'}
-            aoSetarAReferencia={inputAvatar}
+            aoSetarAReferencia={setInputAvatar}
           />
 
           <span onClick={abrirSeletorDeArquivos}>Alterar foto do perfil</span>
